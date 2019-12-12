d3c50dd04ca0e4c65120aee3038de0301198c1c0
Wilson Pereira <wilsonjp.dev@outlook.com>
2019-12-12 10:38:15 -0300

    Tratando erros com excecao personalizada

---
+++ Xadrez/Tabuleiro/TabuleiroException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Xadrez.tabuleiro
+{
+    class TabuleiroException : Exception
+    {
+        public TabuleiroException(string msg) : base(msg)
+        {
+
+        }
+    }
+}
